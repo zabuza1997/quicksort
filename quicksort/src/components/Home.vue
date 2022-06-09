900b18996b30895e66d3fdb83a85c780390d2d02
quang.pham <quang.pham@joinmedia.de>
2022-06-09 11:37:06 +0200

    vue added

---
+++ quicksort/src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <table>
+    <tr>
+      <th
+        v-for="tableHead in tableHeads"
+        class="text-3xl font-bold underline bg-blue-50"
+      >
+        {{ tableHead }}
+      </th>
+    </tr>
+    <tr v-for="(diff, index) in difference">
+      <td>{{ index + 1 }}</td>
+      <td>{{ diff }}</td>
+      <td>{{ points[index] }}</td>
+      <!-- <td>{{ point[index] }}</td> -->
+    </tr>
+  </table>
+  <div class="input-container flex flex-col">
+    <input type="text" placeholder="Tordifferenz" v-model="difference" />
+    <input type="text" placeholder="Punkte" v-model="points" />
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      differenceInput: "",
+      pointsInput: "",
+      tableHeads: ["ID", "Tordifferenz", "Punkte"],
+      difference: [],
+      points: [],
+    };
+  },
+  methods: {},
+};
+</script>
